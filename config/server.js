aaed1531fff4722af88f2b08d9425291f31943ac
Ryan Grunest <ryangrunest@geeko>
2021-07-02 11:27:53 -0700

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bcce5f613d4bee62b0ad5009437f16fb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '09e967d1-8b19-4f89-b5b8-06ef610e4dcc'
+};
